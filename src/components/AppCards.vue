1c4fcf3b833006351585f42b882e33be154854de
V1tale <elativ.c@gmail.com>
2022-11-15 12:31:26 +0100

    applicate le immagini di copertina

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -17,6 +17,7 @@ export default {
             <h2>Movies</h2>
             <div class="card" v-for="(movie) in store.movies">
                 <h3>{{movie.title}}</h3>
+                <img :src="`http://image.tmdb.org/t/p/w500/${movie.poster_path}`" alt="">
                 <h3>{{movie.overview}}</h3>
                 <img v-if="flags.includes(movie.original_language)" :src="`../img/${movie.original_language}.png`">
                 <h3 v-else>language: {{movie.original_language}}</h3>
@@ -28,6 +29,7 @@ export default {
             <h2>Tv-Series</h2>
             <div class="card" v-for="(series) in store.series">
                 <h3>{{series.name}}</h3>
+                <img :src="`http://image.tmdb.org/t/p/w500/${series.poster_path}`" alt="">
                 <h3>{{series.overview}}</h3>
                 <img v-if="flags.includes(series.original_language)" :src="`../img/${series.original_language}.png`">
                 <h3 v-else>language: {{series.original_language}}</h3>
