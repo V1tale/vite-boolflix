b1e1b3dcc81916d86af9b5b1b74ed55ef46d115f
V1tale <elativ.c@gmail.com>
2022-11-15 12:53:47 +0100

    ridimensionamento delle immagini

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -17,9 +17,9 @@ export default {
             <h2>Movies</h2>
             <div class="card" v-for="(movie) in store.movies">
                 <h3>{{movie.title}}</h3>
-                <img :src="`http://image.tmdb.org/t/p/w500/${movie.poster_path}`" alt="">
+                <img class="cover" :src="`http://image.tmdb.org/t/p/w500/${movie.poster_path}`" alt="">
                 <h3>{{movie.overview}}</h3>
-                <img v-if="flags.includes(movie.original_language)" :src="`../img/${movie.original_language}.png`">
+                <img class="flag" v-if="flags.includes(movie.original_language)" :src="`../img/${movie.original_language}.png`">
                 <h3 v-else>language: {{movie.original_language}}</h3>
                 <h3>{{movie.vote_average}}</h3>
             </div>
@@ -29,9 +29,9 @@ export default {
             <h2>Tv-Series</h2>
             <div class="card" v-for="(series) in store.series">
                 <h3>{{series.name}}</h3>
-                <img :src="`http://image.tmdb.org/t/p/w500/${series.poster_path}`" alt="">
+                <img class="cover" :src="`http://image.tmdb.org/t/p/w500/${series.poster_path}`" alt="">
                 <h3>{{series.overview}}</h3>
-                <img v-if="flags.includes(series.original_language)" :src="`../img/${series.original_language}.png`">
+                <img class="flag" v-if="flags.includes(series.original_language)" :src="`../img/${series.original_language}.png`">
                 <h3 v-else>language: {{series.original_language}}</h3>
                 <h3>{{series.vote_average}}</h3>
             </div>
@@ -41,4 +41,12 @@ export default {
 
 </template>
 
-<style></style>
+<style lang="scss">
+    .flag {
+        width:40px;
+        height:30px;
+    }
+    .cover {
+        width:300px;
+    }
+</style>
